a43a6994c333282aa3200b5a95642806c3034ba9
Poornasree1207 <90308500+Poornasree1207@users.noreply.github.com>
2026-04-16 12:35:27 +0530

    added main Spring Boot application class

---
+++ backend/src/main/java/com/bank/app/BankingApplication.java
@@ -0,0 +1,15 @@
+//This is the starter class
+
+package com.bank.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BankingApplication.class, args);
+    }
+}
